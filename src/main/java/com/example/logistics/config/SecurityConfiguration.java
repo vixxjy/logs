b1c92b271571734bd2ba98db8a55b86a402340fd
vixxjy <ajorvictor48@gmail.com>
2022-11-04 12:49:20 +0100

    [setup] basic spring authentication

---
+++ src/main/java/com/example/logistics/config/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.example.logistics.config;
+
+import com.example.logistics.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }
+}
